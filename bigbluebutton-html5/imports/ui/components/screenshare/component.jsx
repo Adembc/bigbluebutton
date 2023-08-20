ff007c247223d38864d0e032099cfa0f9b7baa9a
Adem Baccara <adembc020@gmail.com>
2023-08-20 08:54:15 +0100

    test

--- bigbluebutton-html5/imports/ui/components/screenshare/component.jsx
+++ bigbluebutton-html5/imports/ui/components/screenshare/component.jsx
@@ -86,7 +86,7 @@ class ScreenshareComponent extends React.Component {
     this.state = {
       loaded: false,
       autoplayBlocked: false,
-      mediaFlowing: false,
+      mediaFlowing: true,
       switched: false,
       // Volume control hover toolbar
       showHoverToolBar: false,
